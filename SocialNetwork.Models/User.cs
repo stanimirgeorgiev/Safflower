2d3a5132a5bacc355ab17b2f945cfae02f469a18
Kiril Yankov <k.yankov1980@gmail.com>
2015-08-29 18:04:42 +0300

    Добавил съм основните класове, има да се пооправят някой релации, но като цяло ги качвам за да кажете дали сме на прав път

---
+++ SocialNetwork.Models/User.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+
+namespace FaceBook.Models
+{
+    public  class User: IdentityUser
+    {
+
+
+        public int WallId { get; set; }
+        public virtual Wall Wall { get; set; }
+
+        public int PostId { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+
+        public int CommentId { get; set; }
+        public virtual ICollection<Comment> Coments { get; set; }
+
+        public int PostLikeId { get; set; }
+        public virtual ICollection<PostLike> PostLikes { get; set; }
+
+        public int CommentLikeId { get; set; }
+        public virtual ICollection<CommentLike> CommentLikes { get; set; }
+
+        public int GroupId { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+
+        public int FriendId { get; set; }
+        public virtual ICollection<User> Friends { get; set; }
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(
+            UserManager<User> manager,
+            string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(
+                this,
+                authenticationType);
+
+            return userIdentity;
+        }
+    }
+}
---
+++ SocialNetwork.Models/Wall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace FaceBook.Models
+{
+    public class Wall
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int PostId { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
