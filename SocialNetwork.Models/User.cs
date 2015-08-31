2d90b6b3fc797f16b370ebf244e5391dfc7988ad
Stanimir Georgiev <az.stanimir@gmail.com>
2015-08-31 13:38:32 +0300

    Added Chat entity and removed package from SN.serves

--- SocialNetwork.Models/User.cs
+++ SocialNetwork.Models/User.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace SocialNetwork.Models
 {
