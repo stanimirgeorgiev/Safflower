028a832be26358be099f6bd244da24a1268f04f6
mbencheva <mbencheva@users.noreply.github.com>
2015-09-03 15:31:36 +0300

    recreate WallGroup.cs and WallUser.cs

---
+++ FaceBook.Models/WallGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaceBook.Models
+{
+    public class WallGroup:Wall
+    {
+        public WallGroup():base()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        [Key, ForeignKey("Group")]
+        public Guid Id { get; set; }
+
+        public virtual Group Group { get; set; }
+
+
+        
+
+    }
+}
---
+++ FaceBook.Models/WallUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaceBook.Models
+{
+    public class WallUser:Wall
+    {
+        [Key, ForeignKey("User")]
+        public string Id { get; set; }
+
+        public virtual User User { get; set; }
+
+        
+    }
+}
