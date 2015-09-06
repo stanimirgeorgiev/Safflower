dce2de0bed0a1040084f2c0e430724ce5fb0dfa7
Jordan Georgiev <jordan.t.georgiev@gmail.com>
2015-09-06 12:41:46 +0300

    Added CommentsController endpoints

--- FaceBook.Services/Models/DataModels/CommentDataModel.cs
+++ FaceBook.Services/Models/DataModels/CommentDataModel.cs
@@ -1,4 +1,8 @@
-﻿namespace FaceBook.Services.Models.DataModels
+﻿using System;
+using System.Linq.Expressions;
+using FaceBook.Models;
+
+namespace FaceBook.Services.Models.DataModels
 {
     public class CommentDataModel
     {
@@ -7,5 +11,27 @@
         public string Content { get; set; }
 
         public UserDataModel Author { get; set; }
+
+        public DateTime PostedOn { get; set; }
+
+        public int LikesCount { get; set; }
+
+        public static Expression<Func<Comment, CommentDataModel>> Create
+        {
+            get
+            {
+                return c => new CommentDataModel()
+                {
+                    Id = c.Id,
+                    Content = c.Content,
+                    Author = new UserDataModel()
+                    {
+                        Username = c.User.UserName
+                    },
+                    PostedOn = DateTime.Now,
+                    LikesCount = c.Likes.Count
+                };
+            }
+        } 
     }
 }
