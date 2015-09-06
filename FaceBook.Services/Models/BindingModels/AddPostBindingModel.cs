d405ad4e0905d48aa5e7192758f862abbfe6fda2
Jordan Georgiev <jordan.t.georgiev@gmail.com>
2015-09-06 09:18:27 +0300

    Added AddPostBindingModel

---
+++ FaceBook.Services/Models/BindingModels/AddPostBindingModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FaceBook.Services.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddPostBindingModel
+    {
+        [Required]
+        [MinLength(2)]
+        public string Content { get; set; }
+
+        [Required]
+        public string WallId { get; set; }
+    }
+}
